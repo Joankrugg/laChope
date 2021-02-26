2615db3dff1af6df77a4ce862cf6501d174a01e5
Philippe chambon <joan_krugg@live.fr>
2021-02-26 11:10:13 +0100

    new pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,24 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home, :concept ]
+  skip_before_action :authenticate_user!
 
   def home
   end
 
   def concept
+    @beers = Beer.all
   end
+
+  def target
+    @beers = Beer.all
+  end
+
+  def la_ptite_chopine
+    @beers = Beer.all
+  end
+
+  def design_en_featuring
+    @beers = Beer.all
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show, :edit, :update, :destroy]
   resources :beers
   get 'concept', to: 'pages#concept'
+  get 'target', to: 'pages#target'
+  get 'la_ptite_chopine', to: 'pages#la_ptite_chopine'
+  get 'design_en_featuring', to: 'pages#design_en_featuring'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
