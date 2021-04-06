a963c498ecdb610d5042cb717b66021ad5aeb950
Philippe chambon <joan_krugg@live.fr>
2021-04-06 17:18:01 +0200

    new browsing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :set_targets
 
   def after_sign_in_path_for(resource)
     user_path(current_user)
   end
 
+  def set_targets
+    @beers = Beer.all
+  end
 end
