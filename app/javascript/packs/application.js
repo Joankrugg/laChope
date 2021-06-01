aed3052f019ce596bc41fd314dd3a13198c285a0
Philippe chambon <joan_krugg@live.fr>
2021-06-02 00:35:14 +0200

    PWA

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -32,10 +32,24 @@ import "bootstrap";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
-document.addEventListener('turbolinks:load', () => {
+window.addEventListener('load', () => {
+  navigator.serviceWorker.register('/service-worker.js').then(registration => {
+    console.log('ServiceWorker registered: ', registration);
 
-  // Call your functions here, e.g:
-  // initSelect2();
+    var serviceWorker;
+    if (registration.installing) {
+      serviceWorker = registration.installing;
+      console.log('Service worker installing.');
+    } else if (registration.waiting) {
+      serviceWorker = registration.waiting;
+      console.log('Service worker installed & waiting.');
+    } else if (registration.active) {
+      serviceWorker = registration.active;
+      console.log('Service worker active.');
+    }
+  }).catch(registrationError => {
+    console.log('Service worker registration failed: ', registrationError);
+  });
 });
 
 
