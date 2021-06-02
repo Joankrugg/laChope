d5cb2f7e4e9fbb27164c3fe8e14c0b55d5e458f6
Philippe chambon <joan_krugg@live.fr>
2021-06-02 16:45:11 +0200

    changed ./ to / scope

--- config/initializers/serviceworker.rb
+++ config/initializers/serviceworker.rb
@@ -3,14 +3,14 @@
 Rails.application.configure do
   config.serviceworker.routes.draw do
     # map to assets implicitly
-
+    match "/serviceworker.js"
     match "/manifest.json"
 
     # Examples
     #
     # map to a named asset explicitly
-    match "/proxied-serviceworker.js" => "nested/asset/serviceworker.js"
-    match "/nested/serviceworker.js" => "another/serviceworker.js"
+    # match "/proxied-serviceworker.js" => "nested/asset/serviceworker.js"
+    # match "/nested/serviceworker.js" => "another/serviceworker.js"
     #
     # capture named path segments and interpolate to asset name
     # match "/captures/*segments/serviceworker.js" => "%{segments}/serviceworker.js"
