133980b58f2dc7f0c35a0896c2d3d1d65e34968d
Philippe chambon <joan_krugg@live.fr>
2021-06-02 12:59:45 +0200

    PWA Working at last

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -32,24 +32,10 @@ import "bootstrap";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
-window.addEventListener('load', () => {
-  navigator.serviceWorker.register('/service-worker.js').then(registration => {
-    console.log('ServiceWorker registered: ', registration);
+document.addEventListener('turbolinks:load', () => {
 
-    var serviceWorker;
-    if (registration.installing) {
-      serviceWorker = registration.installing;
-      console.log('Service worker installing.');
-    } else if (registration.waiting) {
-      serviceWorker = registration.waiting;
-      console.log('Service worker installed & waiting.');
-    } else if (registration.active) {
-      serviceWorker = registration.active;
-      console.log('Service worker active.');
-    }
-  }).catch(registrationError => {
-    console.log('Service worker registration failed: ', registrationError);
-  });
+  // Call your functions here, e.g:
+  // initSelect2();
 });
 
 
