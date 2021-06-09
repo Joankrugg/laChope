fafb2b70c5b8750423657dfea17ba88f56741c05
Philippe chambon <joan_krugg@live.fr>
2021-06-09 16:42:26 +0200

    another try

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,2 @@
-if (navigator.serviceWorker) {
-  navigator.serviceWorker.register('/serviceworker.js', { scope: './' })
-    .then(function(reg) {
-      console.log('[Page] Service worker registered!');
-    });
-}
+//= require serviceworker-companion
 
