39a4321b7b5086ffce0b887ee91e8c18fd219b85
Philippe chambon <joan_krugg@live.fr>
2021-10-17 11:15:46 +0200

    Mailer if self.read == false

--- app/models/personal_message.rb
+++ app/models/personal_message.rb
@@ -8,7 +8,7 @@ class PersonalMessage < ApplicationRecord
   private
 
   def notification
-    unless self.read?
+    if self.read == false
       ConversationMailer.with(personal_message: self).send_notification.deliver_now
     end
   end
