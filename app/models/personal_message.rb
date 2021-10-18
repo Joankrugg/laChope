9a8f5b328a4d2874fd53dd8c53c077b08eb36ac2
Philippe chambon <joan_krugg@live.fr>
2021-10-18 13:00:38 +0200

    send mail for each personal message

--- app/models/personal_message.rb
+++ app/models/personal_message.rb
@@ -3,15 +3,21 @@ class PersonalMessage < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
   validates :body, presence: true, allow_blank: true
-  after_commit :notification
+  after_commit :notification, unless: :check_read?
 
 
   private
 
-  def notification
-    unless self.conversation.read?
-      ConversationMailer.with(personal_message: self).send_notification.deliver_now
+  def check_read?
+    if self.conversation.read?
+      true
+    else
+      false
     end
+  end
+
+  def notification
+      ConversationMailer.with(personal_message: self).send_notification.deliver_now
   end
 
 end
