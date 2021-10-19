5da60bfb8c6212e671cd1b116a9e6aa96f6f3027
Philippe chambon <joan_krugg@live.fr>
2021-10-19 02:14:11 +0200

    try in production

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -7,14 +7,14 @@ class ConversationsController < ApplicationController
 
   def show
     pm = @conversation.personal_messages.last
+    pm.update(read: nil)
     if pm.user != current_user
       pm.update(read: true)
       pm.save!
-    else
-      if pm.created_at == pm.updated_at && pm.read == false && pm.created_at > 2.seconds.ago
-        mail = ConversationMailer.with(personal_message: pm).send_notification
-        mail.deliver_now
-      end
+    end
+    if pm.created_at != pm.updated_at && pm.read != true && pm.created_at > 2.seconds.ago
+      mail = ConversationMailer.with(personal_message: pm).send_notification
+      mail.deliver_now
     end
     @personal_message = PersonalMessage.new
   end
