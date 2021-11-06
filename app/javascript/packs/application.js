9a65f581effbbaa8efcc52c70b5b5fe21d226c03
Philippe chambon <joan_krugg@live.fr>
2021-11-06 18:25:50 +0100

    add select2

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,7 +24,12 @@ import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css';
 // internal imports
 import { initMapbox } from '../plugins/init_mapbox';
+import 'select2'
+import 'select2/dist/css/select2.css'
 
+window.addEventListener('DOMContentLoaded', () => {
+  $('.js-states').select2()
+})
 
 // ----------------------------------------------------
 // Note(lewagon): ABOVE IS RAILS DEFAULT CONFIGURATION
