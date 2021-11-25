d286249965d4bb4d2fc76b298e5b22e138b63360
Philippe chambon <joan_krugg@live.fr>
2021-11-25 14:26:33 +0100

    LinkThumbler in model

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -1,12 +1,33 @@
 class Publication < ApplicationRecord
   belongs_to :user
   has_many :answers
+  validates :content, presence: true, allow_blank: true
   has_one_attached :photo
   has_one_attached :video
-  validates :content, presence: true, allow_blank: true
 
+  after_create :link_title
+  after_create :link_favicon
+  after_create :link_description
+  after_create :link_image
 
+  def link_title
+    preview = LinkThumbnailer.generate(link)
+    preview.title
+  end
 
+  def link_favicon
+    preview = LinkThumbnailer.generate(link)
+    preview.favicon
+  end
 
+  def link_description
+    preview = LinkThumbnailer.generate(link)
+    preview.description
+  end
+
+  def link_image
+    preview = LinkThumbnailer.generate(link)
+    preview.images.first.src
+  end
 
 end
