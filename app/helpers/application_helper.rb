c02575500130c0ed6893f99ce14fcdee2db63562
Philippe chambon <joan_krugg@live.fr>
2021-11-30 15:31:16 +0100

    ready to add new table

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,11 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      title:       'Member Login',
+      description: 'Member login page.',
+      keywords:    'Site, Login, Members',
+      separator:   "&mdash;".html_safe,
+    }
+  end
 
 end
