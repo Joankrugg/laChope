2fac8cd66a8c8f82581c1bdd7002811eb97184f6
Philippe Chambon <joan_krugg@live.fr>
2022-03-31 13:10:30 +0200

    typo correction in user model, alt in article image_tag

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,7 @@
 class Recipe < ApplicationRecord
   belongs_to :beer
   belongs_to :unit, optional: true
-  belongs_to :action
+  belongs_to :action, optional: true
   belongs_to :duration_unit, optional: true
   belongs_to :product_type, optional: true
   validates :step, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -23,7 +23,7 @@ class User < ApplicationRecord
   has_many :tastings
   has_many :beers, through: :tastings
   has_many :authored_conversations, class_name: 'Conversation', foreign_key: 'author_id'
-  has_many :received_conversations, class_name: 'Conversation', foreign_key: 'received_id'
+  has_many :received_conversations, class_name: 'Conversation', foreign_key: 'receiver_id'
   has_many :personal_messages, dependent: :destroy
   has_many :typical_beers, dependent: :destroy
   has_many :projects, dependent: :destroy
